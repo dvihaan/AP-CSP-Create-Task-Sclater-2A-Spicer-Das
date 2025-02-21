81daf9fdee4f1b16ad4159e45012f4ff67c6df81
dvihaan <dvihaan1@gmail.com>
2025-02-21 14:13:59 -0500

    Initial commit/git test

--- Main Code.py
+++ Main Code.py
@@ -1,5 +1,6 @@
 import requests 
 
+#Testing git commit
 def get_weather(latitude, longitude):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
     response = requests.get(url)
